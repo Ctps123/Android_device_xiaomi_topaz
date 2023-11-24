fab7df3ca34d6a6ffff60244e729cb1681a14e56
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-11-23 23:57:00 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_topaz.mk
+    $(LOCAL_DIR)/lineage_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_topaz-eng \
-    evolution_topaz-userdebug \
-    evolution_topaz-user
+    lineage_topaz-eng \
+    lineage_topaz-userdebug \
+    lineage_topaz-user
