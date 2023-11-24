af2524a75db994aa5380fd15f074f5c1bf5e9f7c
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-11-24 08:14:07 -0300

    Update rising_topaz.mk

--- rising_topaz.mk
+++ rising_topaz.mk
@@ -5,8 +5,8 @@
 #
 
 # Inherit from products. Most specific first.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 #$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
