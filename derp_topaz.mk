56288d27f72ee0f787080f184ba0cdcb8d69950d
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-11-25 00:41:58 -0300

    Update and rename rising_topaz.mk to derp_topaz.mk

--- derp_topaz.mk
+++ derp_topaz.mk
@@ -9,30 +9,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-#$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-$(call inherit-product, vendor/rising/config/rising.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+#$(call inherit-product, vendor/rising/config/rising.mk)
 # Inherit from topaz device.
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
 # Flags rom
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-#TARGET_USES_BLUR := true
-#USE_PIXEL_CHARGING := true
-#TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_PREBUILT_PIXEL_LAUNCHER := true
-TARGET_ENABLE_PIXEL_FEATURES := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-RISING_MAINTAINER :=scorpion
-RISING_CHIPSET := Snapdragon685
-RISING_STORAGE := 128GB
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+#WITH_GMS := true
+#TARGET_CORE_GMS := true
+#TARGET_PREBUILT_PIXEL_LAUNCHER := true
+#TARGET_ENABLE_PIXEL_FEATURES := true
+#TARGET_USE_GOOGLE_TELEPHONY := true
+#RISING_MAINTAINER :=scorpion
+#RISING_CHIPSET := Snapdragon685
+#RISING_STORAGE := 128GB
 #RISING_RAM := 6GB
-RISING_BATTERY := 5000mAh
+#RISING_BATTERY := 5000mAh
 
 # Device identifier
-PRODUCT_NAME := rising_topaz
+PRODUCT_NAME := derp_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 12 4G
