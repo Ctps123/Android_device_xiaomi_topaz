a5c33547bd5adee5de35954427dea17217f0e976
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-11-30 22:18:43 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lmodroid_topaz.mk
+    $(LOCAL_DIR)/lineage_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    lmodroid_topaz-eng \
-    lmodroid_topaz-userdebug \
-    lmodroid_topaz-user
+    lineage_topaz-eng \
+    lineage_topaz-userdebug \
+    lineage_topaz-user
